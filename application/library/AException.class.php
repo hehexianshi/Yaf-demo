8909b094222167748ab19b8117623fa86f83f17d
lidong <zhubaiying@126.com>
2012-05-10 17:03:24 +0800

    添加注视

---
+++ application/library/AException.class.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ *  AException class file
+ *  @auther li dong <zhubaiying@gmail.com>
+ *  @version $Id: AException.class.php 2012-5-10 $
+ */
+
+
+class AException extends exception
+{
+
+}
