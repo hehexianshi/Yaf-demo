d737f77f001e7253cf4f4fd87807940ba67ae8a5
lidong <zhubaiying@126.com>
2012-05-10 17:22:22 +0800

    choose file name

---
+++ application/library/ACDb.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Db class file.
+ * @author li dong <zhubaiying@gmail.com>
+ * @version $Id Db.class.php 2012-5-10 $
+ */
+
+class ACDb
+{
+
+}
---
+++ application/library/AFException.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * AException fun file.
+ * @author li dong <zhubaiying@gmail.com>
+ * @version $Id: AException.func.php 2012-5-10 $
+ */
+function AFException($error, $errstr, $errfile, $errline)
+{
+    /**
+     * error 是否写入文件 
+     */
+      
+    $inputFile = defined('ERROR_PUT_FILE') ? true : false;
+    switch ($error) {
+        case YAF_ERR_NOTFOUND_CONTROLLER;
+        case YAF_ERR_NOTFOUND_MODULE;
+        case YAF_ERR_NOTFOUND_ACTION;
+            $err_str = 'not found';
+            header($err_str);
+        break
+            $err_str = "Unkown error type:[$error] $errstr<br>\n";
+            break;
+    }
+    if(!$inputFile){
+        /**
+         *  写入文件
+         */
+    }
+    return true;
+}
