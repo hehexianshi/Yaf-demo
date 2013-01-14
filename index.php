83bb2752cbba1c6fc0f5363a4538275d4ff12ed2
hehexianshi <zhubaiying@126.com>
2013-01-14 23:36:28 +0800

    test

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
  * index file.
  * @author lidong <zhubaiying@gmail.com>
  * @version $Id    index.php   2012-5-10 $
+ * test
  */
 define("APP_PATH", dirname(__FILE__));
 $app = new Yaf_Application(APP_PATH."/conf/application.ini");
