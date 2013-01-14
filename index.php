2ae2dd90e21c0427ade4abd1a80a4a16ea1e9bd7
cindyzhu <zhubaiying1110@163.com>
2013-01-15 00:01:24 +0800

    create

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
  * index file.
  * @author lidong <zhubaiying@gmail.com>
  * @version $Id    index.php   2012-5-10 $
- * test
+ * test demo
  */
 define("APP_PATH", dirname(__FILE__));
 $app = new Yaf_Application(APP_PATH."/conf/application.ini");
